b7bdca70a4df35a55fed9c159423a4483beb4d24
ElectricNinja315 <suzac314@gmail.com>
2020-10-09 22:32:46 -0700

    in

---
+++ index.js
@@ -0,0 +1,30 @@
+
+function openTheology() {
+    if (document.getElementById("theologyTopics").style.display == "block") {
+        document.getElementById("theologyTopics").style.display = "none";
+    } else {
+        document.getElementById("theologyTopics").style.display = "block";
+    }
+}
+
+function openEthics() {
+    if (document.getElementById("ethicsTopics").style.display == "block") {
+        document.getElementById("ethicsTopics").style.display = "none";
+    } else {
+        document.getElementById("ethicsTopics").style.display = "block";
+    }
+}
+function openMetaphysics() {
+    if (document.getElementById("metaphysicsTopics").style.display == "block") {
+        document.getElementById("metaphysicsTopics").style.display = "none";
+    } else {
+        document.getElementById("metaphysicsTopics").style.display = "block";
+    }
+}
+function openPolitics() {
+    if (document.getElementById("politicsTopics").style.display == "block") {
+        document.getElementById("politicsTopics").style.display = "none";
+    } else {
+        document.getElementById("politicsTopics").style.display = "block";
+    }
+}
